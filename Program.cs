701b6f607ab401958a18ac16776d63c5a042b8fb
ehodzic6 <ehodzic6@etf.unsa.ba>
2025-02-06 23:31:08 +0100

    Registered services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using GameOfLife.Context;
+using GameOfLife.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=gameoflife.db"));
+
+builder.Services.AddScoped<IHumanRepository, HumanRepository>();
 
 var app = builder.Build();
 
