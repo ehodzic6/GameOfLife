cef5dece9c7be4cf5df4dde8d28115bf74553048
ehodzic6 <ehodzic6@etf.unsa.ba>
2025-02-06 22:50:36 +0100

    Added Human model

---
+++ Models/Human.cs
@@ -0,0 +1,29 @@
+﻿namespace LifeGame.Models
+{
+    public class Human
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double Fitness { get; set; }
+        public double Hunger { get; set; }
+        public double Engineering { get; set; }
+        public double BrainCapacity { get; set; }
+        public double Fun { get; set; }
+
+        public Human(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Health = 50;
+            Fitness = 50;
+            Hunger = 50;
+            Engineering = 50;
+            BrainCapacity = 50;
+            Fun = 50;
+
+        }
+
+        public Human() { }
+    }
+}
